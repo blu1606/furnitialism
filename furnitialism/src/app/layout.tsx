720ce191c59e41524d2c3c16e5f13bc6da4cc7c4
blu1606 <dongthanhquandtq@gmail.com>
2026-03-13 19:52:24 +0700

    feat: update layout

--- furnitialism/src/app/layout.tsx
+++ furnitialism/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Furnitialism - 3D Interior Design",
-  description: "Experience interiors like never before with Furnitialism's immersive 3D technology and AI-driven simulations.",
+  title: "Furnitialism - 3D Interior Design & AI Furniture Shop",
+  description: "Experience the future of furniture with Furnitialism's immersive 3D technology and AI-driven interior simulations.",
 };
 
 export default function RootLayout({
@@ -12,14 +12,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=block"
         />
       </head>
-      <body className="antialiased bg-background-light dark:bg-background-dark text-stone-900 dark:text-white">
+      <body className="antialiased bg-background-light text-stone-900">
         {children}
       </body>
     </html>
